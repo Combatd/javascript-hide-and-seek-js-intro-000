0ef994c3bb5bef4e5e06c4c837268c8e796d4387
Mark Calvelo <combatdstar@gmail.com>
2019-09-01 13:00:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 /*Define a function getFirstSelector(selector), which accepts a selector and returns the first element that matches.*/
 function getFirstSelector(selector) {
-  return document.querySelector(selector);
+  let firstEle = document.querySelector(selector);
+  return firstEle;
 }
 
 /*Define a function nestedTarget() that pulls a .target out of #nested (# is used for IDs in selectors — but you knew that because you read the docs, right? :) ). (Note that in index.html #nested and .target just happen to be divs. This method should work with arbitrary elements.)*/
