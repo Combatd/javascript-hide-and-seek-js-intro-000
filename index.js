dd47121fd85016e9607282e9fc91ccd2cce255a5
Mark Calvelo <combatdstar@gmail.com>
2019-09-01 12:46:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function nestedTarget() {
 
 /*Define a function increaseRankBy(n) that increases the ranks in all of the .ranked-lists by n. (You might need to make use of parseInt()*/
 function increaseRankBy(n) {
-  let rankedLists = document.querySelectorAll(.ranked-lists);
+  let rankedLists = document.querySelectorAll('.ranked-lists');
 }
 
 /*Define a function deepestChild() that pulls out the most deeply nested child element from div#grand-node. (Remember, you can iterate over elements and call querySelector() and querySelectorAll() on them. This is challenging to implement correctly, but not beyond your ability!)*/
